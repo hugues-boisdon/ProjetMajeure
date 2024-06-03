faf361851d4eb8c04f1286511b9981eeb0e5c944
Hugues Boisdon <127095418+hugues-boisdon@users.noreply.github.com>
2024-06-03 09:24:46 +0200

    Integration

--- front-end/script.js
+++ front-end/script.js
@@ -1,15 +1,45 @@
-// Variables Declaration
-let map
+// Initialize the map
+var map = L.map('map').setView([45.75, 4.85], 13); // Coordinates of Lyon
 
-// Leaflet map Initialisation to #map div
-map = L.map('map').setView([51.505, -0.09], 13); // London Map (for initial testing)
+// Add OpenStreetMap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
+var markers = [];
 
-// Leaflet event functions
-function onMapClick(e) {
-    alert("You clicked the map at " + e.latlng);
+// Fetch fire data from the API
+function fetchFireData() {
+    fetch('http://tp.cpe.fr:8081/fire')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(fire => {
+                var marker = L.marker([fire.coordinates.lat, fire.coordinates.lng]);
+                marker.bindPopup(
+                    `Feu de classe ${fire.type}<br>Intensité: ${fire.intensity}<br>Étendue: ${fire.extent}m²`
+                );
+                markers.push({ marker: marker, type: fire.type });
+                marker.addTo(map);
+            });
+        });
 }
 
+// Function to filter markers
+function filterMarkers(type) {
+    markers.forEach(item => {
+        if (type === 'all' || item.type === type) {
+            item.marker.addTo(map);
+        } else {
+            map.removeLayer(item.marker);
+        }
+    });
+}
 
-// Leaflet event to event functions pairing
-map.on('click', onMapClick);
+// Event listener for filter selection
+document.getElementById('fireType').addEventListener('change', function(event) {
+    var type = event.target.value;
+    filterMarkers(type);
+});
+
+// Initial data fetch
+fetchFireData();
