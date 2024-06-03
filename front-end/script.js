99194c80ce06f651fa0321cddb13f653ce19f8d5
Hugues Boisdon <127095418+hugues-boisdon@users.noreply.github.com>
2024-06-03 09:13:34 +0200

    intial leaflet

--- front-end/script.js
+++ front-end/script.js
@@ -1,5 +1,15 @@
 // Variables Declaration
 let map
 
-// Leaflet map Integration
-map = L.map('map').setView([51.505, -0.09], 13);
+// Leaflet map Initialisation to #map div
+map = L.map('map').setView([51.505, -0.09], 13); // London Map (for initial testing)
+
+
+// Leaflet event functions
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+
+
+// Leaflet event to event functions pairing
+map.on('click', onMapClick);
